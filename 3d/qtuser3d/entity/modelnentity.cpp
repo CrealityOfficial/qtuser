eefbe805dee47639305a4058f3ff6c9350a67246
weizhixiao <weizhixiao@creality.com>
2022-08-27 17:02:11 +0800

    feat:[model]模型在平台上的投射改为阴影
    Change-Id: I866d249f7a7ff079ca626da2377f7464949fe6d0

--- 3d/qtuser3d/entity/modelnentity.cpp
+++ 3d/qtuser3d/entity/modelnentity.cpp
@@ -10,6 +10,7 @@ namespace qtuser_3d
 		m_boxEntity = new qtuser_3d::BoxEntity();
 		m_boxEntity->setColor(QVector4D(0.306f, 0.702f, 0.769f, 1.0f));
 
+
 		m_convexXYEntity = new qtuser_3d::LineEntity();
 		m_convexXYEntity->setObjectName("ModelNEntity");
 
@@ -48,7 +49,7 @@ namespace qtuser_3d
 	void ModelNEntity::setBoxVisibility(bool visible)
 	{
 		m_boxEntity->setParent(visible ? (Qt3DCore::QNode*)this->parent() : nullptr);
-		////m_convexXYEntity->setParent(visible ? (Qt3DCore::QNode*)this->parent() : nullptr);
+//      m_convexXYEntity->setParent(visible ? (Qt3DCore::QNode*)this->parent() : nullptr);
 	}
 
 	void ModelNEntity::updateBoxLocal(const Box3D& box, const QMatrix4x4& parentMatrix)
