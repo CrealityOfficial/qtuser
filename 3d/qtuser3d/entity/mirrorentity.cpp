76e852f1e8a043a831953e690d114d06c9e3ece6
fanjunhong <fanjunhong@piocreat3d.com>
2023-04-20 16:28:06 +0800

    feat:[qtuser3d] 完善镜像 Entity
    Change-Id: I82472795bce2ca5d21f00c6127f88db1dbd67e47

--- 3d/qtuser3d/entity/mirrorentity.cpp
+++ 3d/qtuser3d/entity/mirrorentity.cpp
@@ -1,42 +1,186 @@
 #include "mirrorentity.h"
 
+#include "qtuser3d/utils/primitiveshapecache.h"
+#include "qtuser3d/geometry/basicshapecreatehelper.h"
+
 namespace qtuser_3d {
 
-MirrorEntity::MirrorEntity(Qt3DCore::QNode* parent,
-                           MirrorHandler x_mirror_handler,
-                           MirrorHandler y_mirror_handler,
-                           MirrorHandler z_mirror_handler)
+namespace {
+
+QMatrix4x4 CreateRotetedScaledMatrix(QVector3D&& to_direction) {
+  static const QVector3D FROM_DIRECTION{ 0.0f, 1.0f, 0.0f };
+
+  to_direction.normalize();
+  
+  QMatrix4x4 matrix;
+  matrix.rotate(QQuaternion::rotationTo(FROM_DIRECTION, to_direction));
+  matrix.scale(70.0f, 70.0f, 70.0f);
+
+  return matrix;
+}
+
+}
+
+MirrorEntity::MirrorEntity( QPointer<CameraController> camera_controller,
+                           Qt3DCore::QNode* parent)
     : BasicEntity(parent)
-    , x_mirror_handler_(x_mirror_handler)
-    , y_mirror_handler_(y_mirror_handler)
-    , z_mirror_handler_(z_mirror_handler) {
+    , camera_controller_(camera_controller)
+    , space_box_(QVector3D{ 0.0f, 0.0f, 0.0f })
+    , transform_(new Qt3DCore::QTransform{ this })
+    , geometry_(BasicShapeCreateHelper::createInstructions(0.0f, 1.0f, 0.1f, 0.4f, this))
+    , x_positive_entity_(std::make_unique<qtuser_3d::ManipulateEntity>(this))
+    , y_negative_entity_(std::make_unique<qtuser_3d::ManipulateEntity>(this))
+    , z_positive_entity_(std::make_unique<qtuser_3d::ManipulateEntity>(this))
+    , x_negative_entity_(std::make_unique<qtuser_3d::ManipulateEntity>(this))
+    , y_positive_entity_(std::make_unique<qtuser_3d::ManipulateEntity>(this))
+    , z_negative_entity_(std::make_unique<qtuser_3d::ManipulateEntity>(this))
+    , x_positive_pickable_(std::make_shared<qtuser_3d::ManipulatePickable>(this))
+    , x_negative_pickable_(std::make_shared<qtuser_3d::ManipulatePickable>(this))
+    , y_positive_pickable_(std::make_shared<qtuser_3d::ManipulatePickable>(this))
+    , y_negative_pickable_(std::make_shared<qtuser_3d::ManipulatePickable>(this))
+    , z_positive_pickable_(std::make_shared<qtuser_3d::ManipulatePickable>(this))
+    , z_negative_pickable_(std::make_shared<qtuser_3d::ManipulatePickable>(this)) {
+  addComponent(transform_.get());
 
+  x_positive_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{  1.0f,  0.0f,  0.0f }));
+  x_negative_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{ -1.0f,  0.0f,  0.0f }));
+  y_positive_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{  0.0f,  1.0f,  0.0f }));
+  y_negative_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{  0.0f, -1.0f,  0.0f }));
+  z_positive_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{  0.0f,  0.0f,  1.0f }));
+  z_negative_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{  0.0f,  0.0f, -1.0f }));
+
+  x_positive_entity_->setGeometry(geometry_.get());
+  x_negative_entity_->setGeometry(geometry_.get());
+  y_positive_entity_->setGeometry(geometry_.get());
+  y_negative_entity_->setGeometry(geometry_.get());
+  z_positive_entity_->setGeometry(geometry_.get());
+  z_negative_entity_->setGeometry(geometry_.get());
+
+  x_positive_entity_->setColor(QVector4D{ 1.0f, 0.0f, 0.0f, 1.0f });
+  x_negative_entity_->setColor(QVector4D{ 1.0f, 0.0f, 0.0f, 1.0f });
+  y_positive_entity_->setColor(QVector4D{ 0.0f, 1.0f, 0.0f, 1.0f });
+  y_negative_entity_->setColor(QVector4D{ 0.0f, 1.0f, 0.0f, 1.0f });
+  z_positive_entity_->setColor(QVector4D{ 0.0f, 0.0f, 1.0f, 1.0f });
+  z_negative_entity_->setColor(QVector4D{ 0.0f, 0.0f, 1.0f, 1.0f });
+
+  x_positive_entity_->setChangeColor(QVector4D{ 1.0f, 0.79f, 0.0f, 1.0f });
+  x_negative_entity_->setChangeColor(QVector4D{ 1.0f, 0.79f, 0.0f, 1.0f });
+  y_positive_entity_->setChangeColor(QVector4D{ 1.0f, 0.79f, 0.0f, 1.0f });
+  y_negative_entity_->setChangeColor(QVector4D{ 1.0f, 0.79f, 0.0f, 1.0f });
+  z_positive_entity_->setChangeColor(QVector4D{ 1.0f, 0.79f, 0.0f, 1.0f });
+  z_negative_entity_->setChangeColor(QVector4D{ 1.0f, 0.79f, 0.0f, 1.0f });
+  
+  x_positive_entity_->setLightEnable(true);
+  x_negative_entity_->setLightEnable(true);
+  y_positive_entity_->setLightEnable(true);
+  y_negative_entity_->setLightEnable(true);
+  z_positive_entity_->setLightEnable(true);
+  z_negative_entity_->setLightEnable(true);
+  
+  x_positive_entity_->setMethod(1);
+  x_negative_entity_->setMethod(1);
+  y_positive_entity_->setMethod(1);
+  y_negative_entity_->setMethod(1);
+  z_positive_entity_->setMethod(1);
+  z_negative_entity_->setMethod(1);
+  
+  x_positive_pickable_->setPickableEntity(x_positive_entity_.get());
+  x_negative_pickable_->setPickableEntity(x_negative_entity_.get());
+  y_positive_pickable_->setPickableEntity(y_positive_entity_.get());
+  y_negative_pickable_->setPickableEntity(y_negative_entity_.get());
+  z_positive_pickable_->setPickableEntity(z_positive_entity_.get());
+  z_negative_pickable_->setPickableEntity(z_negative_entity_.get());
+
+  x_positive_pickable_->setEnableSelect(false);
+  x_negative_pickable_->setEnableSelect(false);
+  y_positive_pickable_->setEnableSelect(false);
+  y_negative_pickable_->setEnableSelect(false);
+  z_positive_pickable_->setEnableSelect(false);
+  z_negative_pickable_->setEnableSelect(false);
 }
 
-MirrorEntity::~MirrorEntity() {}
-
-auto MirrorEntity::getXMirrorHandler() const -> MirrorHandler {
-  return x_mirror_handler_;
+QPointer<CameraController> MirrorEntity::getCameraController() const {
+  return camera_controller_;
 }
 
-void MirrorEntity::setXMirrorHandler(MirrorHandler x_mirror_handler) {
-  x_mirror_handler_ = x_mirror_handler;
+void MirrorEntity::setCameraController(QPointer<CameraController> camera_controller) {
+  camera_controller_ = camera_controller;
 }
 
-auto MirrorEntity::getYMirrorHandler() const -> MirrorHandler {
-  return y_mirror_handler_;
+Box3D MirrorEntity::getSpaceBox() const {
+  return space_box_;
 }
 
-void MirrorEntity::setYMirrorHandler(MirrorHandler y_mirror_handler) {
-  y_mirror_handler_ = y_mirror_handler;
+void MirrorEntity::setSpaceBox(const Box3D& space_box) {
+  if (space_box_ == space_box) { return; }
+
+  space_box_ = space_box;
+  
+  QMatrix4x4 matrix;
+  matrix.translate(space_box_.center());
+  matrix.scale(1.0f);
+  
+  transform_->setMatrix(matrix);
 }
 
-auto MirrorEntity::getZMirrorHandler() const -> MirrorHandler {
-  return z_mirror_handler_;
+float MirrorEntity::getScaleFactor() const {
+  return transform_->scale();
 }
 
-void MirrorEntity::setZMirrorHandler(MirrorHandler z_mirror_handler) {
-  z_mirror_handler_ = z_mirror_handler;
+void MirrorEntity::setScaleFactor(float scale_factor) {
+  transform_->setScale(scale_factor);
+}
+
+std::weak_ptr<Pickable> MirrorEntity::xPositivePickable() const {
+  return std::dynamic_pointer_cast<Pickable>(x_positive_pickable_);
+}
+
+std::weak_ptr<Pickable> MirrorEntity::xNegativePickable() const {
+  return std::dynamic_pointer_cast<Pickable>(x_negative_pickable_);
+}
+
+std::weak_ptr<Pickable> MirrorEntity::yPositivePickable() const {
+  return std::dynamic_pointer_cast<Pickable>(y_positive_pickable_);
+}
+
+std::weak_ptr<Pickable> MirrorEntity::yNegativePickable() const {
+  return std::dynamic_pointer_cast<Pickable>(y_negative_pickable_);
+}
+
+std::weak_ptr<Pickable> MirrorEntity::zPositivePickable() const {
+  return std::dynamic_pointer_cast<Pickable>(z_positive_pickable_);
+}
+
+std::weak_ptr<Pickable> MirrorEntity::zNegativePickable() const {
+  return std::dynamic_pointer_cast<Pickable>(z_negative_pickable_);
+}
+
+void MirrorEntity::attach() {
+  if (camera_controller_ != nullptr) {
+    connect(camera_controller_, &CameraController::signalCameraChaged,
+            this , &MirrorEntity::onCameraChanged);
+
+    onCameraChanged(camera_controller_->getViewPosition(),
+                    camera_controller_->getViewupVector());
+  }
+}
+
+void MirrorEntity::detach() {
+  if (camera_controller_ != nullptr) {
+    camera_controller_->disconnect(this);
+    this->disconnect(camera_controller_);
+  }
+}
+
+void MirrorEntity::onCameraChanged(QVector3D position, QVector3D up_vector) {
+  std::ignore = up_vector;
+
+  x_positive_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{  position.x(), 0.0f, 0.0f }));
+  x_negative_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{ -position.x(), 0.0f, 0.0f }));
+  y_positive_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{ 0.0f,  position.y(), 0.0f }));
+  y_negative_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{ 0.0f, -position.y(), 0.0f }));
+  z_positive_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{ 0.0f, 0.0f,  position.z() }));
+  z_negative_entity_->setPose(CreateRotetedScaledMatrix(QVector3D{ 0.0f, 0.0f, -position.z() }));
 }
 
 }  // namespace qtuser_3d
--- 3d/qtuser3d/entity/mirrorentity.h
+++ 3d/qtuser3d/entity/mirrorentity.h
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <memory>
 #include <functional>
 
-#include <Qt3DCore/QNode>
+#include <QNode>
+#include <QEntity>
+#include <QPointer>
+#include <QGeometry>
+#include <QTransform>
 
 #include "qtuser3d/qtuser3dexport.h"
 #include "qtuser3d/entity/basicentity.h"
+#include "qtuser3d/entity/manipulateentity.h"
+#include "qtuser3d/module/manipulatepickable.h"
+#include "qtuser3d/camera/cameracontroller.h"
+#include "qtuser3d/math/box3d.h"
 
 namespace qtuser_3d {
 
@@ -13,33 +22,59 @@ class QTUSER_3D_API MirrorEntity : public BasicEntity {
   Q_OBJECT;
 
 public:
-  using MirrorHandler = std::function<void(void)>;
+  explicit MirrorEntity(QPointer<CameraController> camera_controller = nullptr,
+                        Qt3DCore::QNode* parent = nullptr);
+  virtual ~MirrorEntity() = default;
 
 public:
-  explicit MirrorEntity(Qt3DCore::QNode* parent = nullptr,
-                        MirrorHandler x_mirror_handler = nullptr,
-                        MirrorHandler y_mirror_handler = nullptr,
-                        MirrorHandler z_mirror_handler = nullptr);
-  virtual ~MirrorEntity();
+  QPointer<CameraController> getCameraController() const;
+  void setCameraController(QPointer<CameraController> camera_controller);
 
 public:
-  auto getXMirrorHandler() const -> MirrorHandler;
-  void setXMirrorHandler(MirrorHandler x_mirror_handler);
+  Box3D getSpaceBox() const;
+  void setSpaceBox(const Box3D& space_box);
 
 public:
-  auto getYMirrorHandler() const -> MirrorHandler;
-  void setYMirrorHandler(MirrorHandler y_mirror_handler);
+  float getScaleFactor() const;
+  void setScaleFactor(float scale_factor);
 
 public:
-  auto getZMirrorHandler() const -> MirrorHandler;
-  void setZMirrorHandler(MirrorHandler z_mirror_handler);
+  std::weak_ptr<Pickable> xPositivePickable() const;
+  std::weak_ptr<Pickable> xNegativePickable() const;
+
+  std::weak_ptr<Pickable> yPositivePickable() const;
+  std::weak_ptr<Pickable> yNegativePickable() const;
+
+  std::weak_ptr<Pickable> zPositivePickable() const;
+  std::weak_ptr<Pickable> zNegativePickable() const;
+
+public:
+  void attach();
+  void detach();
 
 private:
-  MirrorHandler x_mirror_handler_;
-  MirrorHandler y_mirror_handler_;
-  MirrorHandler z_mirror_handler_;
+  Q_SLOT void onCameraChanged(QVector3D position, QVector3D up_vector);
 
+private:
+  QPointer<CameraController> camera_controller_;
+  Box3D space_box_;
+
+  std::unique_ptr<Qt3DCore::QTransform> transform_;
+  std::unique_ptr<Qt3DRender::QGeometry> geometry_;
   
+  std::unique_ptr<ManipulateEntity> x_positive_entity_;
+  std::unique_ptr<ManipulateEntity> x_negative_entity_;
+  std::unique_ptr<ManipulateEntity> y_positive_entity_;
+  std::unique_ptr<ManipulateEntity> y_negative_entity_;
+  std::unique_ptr<ManipulateEntity> z_positive_entity_;
+  std::unique_ptr<ManipulateEntity> z_negative_entity_;
+
+  std::shared_ptr<ManipulatePickable> x_positive_pickable_;
+  std::shared_ptr<ManipulatePickable> x_negative_pickable_;
+  std::shared_ptr<ManipulatePickable> y_positive_pickable_;
+  std::shared_ptr<ManipulatePickable> y_negative_pickable_;
+  std::shared_ptr<ManipulatePickable> z_positive_pickable_;
+  std::shared_ptr<ManipulatePickable> z_negative_pickable_;
 };
 
 }  // namespace qtuser_3d
