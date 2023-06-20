3bbda90382524138a06310bb113db802a5a0aec2
CXSW1\106251 <suruochang@creality.com>
2023-06-20 18:02:03 +0800

    revert[translate entity]:解决geometry为空导致奔溃的问题
    Change-Id: Id0031e107feb906a034b4b0271662153a461d6b5

--- 3d/qtuser3d/entity/translatehelperentity.cpp
+++ 3d/qtuser3d/entity/translatehelperentity.cpp
@@ -153,12 +153,10 @@ namespace qtuser_3d
 		if (shapeType == IndicatorType::ARROW)
 		{
 			geometry = PRIMITIVESHAPE("arrow");
-			geometry->setParent((Qt3DCore::QNode*)nullptr);
 		}
 		else if (shapeType == IndicatorType::CUBE)
 		{
 			geometry = PRIMITIVESHAPE("scaleindicator");
-			geometry->setParent((Qt3DCore::QNode*)nullptr);
 		}
 
 		if (helperType & HELPERTYPE_AXIS_X)
